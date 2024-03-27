5df7c4ae1408e8cb2889d640a029efb5fdf52ab1
Unpublished <Unpublished@users.noreply.github.com>
2024-03-27 10:20:06 +0100

    Switch pid_t to i32 (#6)
    * switch pid_t to i32
    
    * Include windows in CI

--- src/entry.rs
+++ src/entry.rs
@@ -1,4 +1,3 @@
-use libc::pid_t;
 use std::convert::TryFrom;
 use std::os::raw::c_short;
 use std::str;
@@ -16,7 +15,7 @@ pub enum UtmpEntry {
     /// Change in system run-level (see `init(8)`)
     RunLevel {
         /// PID of the init process
-        pid: pid_t,
+        pid: i32,
         /// Kernel version
         kernel_version: String,
         /// Time entry was made
@@ -43,14 +42,14 @@ pub enum UtmpEntry {
     /// Process spawned by `init(8)`
     InitProcess {
         /// PID of the init process
-        pid: pid_t,
+        pid: i32,
         /// Time entry was made
         time: OffsetDateTime,
     },
     /// Session leader process for user login
     LoginProcess {
         /// PID of the login process
-        pid: pid_t,
+        pid: i32,
         /// Device name of tty
         line: String,
         /// Username
@@ -63,7 +62,7 @@ pub enum UtmpEntry {
     /// Normal process
     UserProcess {
         /// PID of login process
-        pid: pid_t,
+        pid: i32,
         /// Device name of tty
         line: String,
         /// Username
@@ -71,7 +70,7 @@ pub enum UtmpEntry {
         /// Hostname for remote login
         host: String,
         /// Session ID (`getsid(2)`)
-        session: pid_t,
+        session: i32,
         /// Time entry was made
         time: OffsetDateTime,
         // TODO: Figure out the correct byte order to parse the address
@@ -80,7 +79,7 @@ pub enum UtmpEntry {
     /// Terminated process
     DeadProcess {
         /// PID of the terminated process
-        pid: pid_t,
+        pid: i32,
         /// Device name of tty
         line: String,
         /// Time entry was made
@@ -159,7 +158,7 @@ impl<'a> TryFrom<&'a utmp64> for UtmpEntry {
                 line: string_from_bytes(&from.ut_line).map_err(UtmpError::InvalidLine)?,
                 user: string_from_bytes(&from.ut_user).map_err(UtmpError::InvalidUser)?,
                 host: string_from_bytes(&from.ut_host).map_err(UtmpError::InvalidHost)?,
-                session: from.ut_session as pid_t,
+                session: from.ut_session as i32,
                 time: time_from_tv(from.ut_tv)?,
             },
             utmp_raw::DEAD_PROCESS => UtmpEntry::DeadProcess {
